a4113484f274684955fd050a0a7640a5ecf2d268
Ryan Martin <ryanmartin@Ryans-MacBook-Pro.local>
2019-12-15 01:25:42 -0800

    Updates

---
+++ precache-manifest.9f093d2d2920e1899b06d3ea17f9cdbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f962561fd497c9f7ee3401739b5a482d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2ec7b9d7c13816b800a",
+    "url": "/monsters-rolodex/static/css/main.bcbfd007.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2ec7b9d7c13816b800a",
+    "url": "/monsters-rolodex/static/js/main.ec06532d.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
